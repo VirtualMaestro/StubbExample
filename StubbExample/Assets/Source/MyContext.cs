2d1bc8b09508d36dd2870751d1be52c6717fb8bb
VirtualMaestro <virtualmaestro@gmail.com>
2019-12-11 12:08:47 +0100

    Forgotten class

---
+++ StubbExample/Assets/Source/MyContext.cs
@@ -0,0 +1,13 @@
+﻿using Leopotam.Ecs;
+using Source;
+using StubbUnity.Contexts;
+
+public class MyContext : UnityContext
+{
+    protected override IEcsSystem InitUserSystems()
+    {
+        var initUserSystems = new EcsSystems(World);
+        initUserSystems.Add(new MainSystem());
+        return initUserSystems;
+    }
+}
