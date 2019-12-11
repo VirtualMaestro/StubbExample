b70042c21fcff4580a3a0a5f11692edfb209ea69
VirtualMaestro <virtualmaestro@gmail.com>
2019-12-11 12:08:04 +0100

    Update example with the scenes

---
+++ StubbExample/Assets/Source/MainSystem.cs
@@ -0,0 +1,34 @@
+﻿using Leopotam.Ecs;
+using StubbFramework.Extensions;
+using UnityEngine;
+
+namespace Source
+{
+    public class MainSystem : IEcsRunSystem
+    {
+        private EcsWorld _world;
+        
+        public void Run()
+        {
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                _world.LoadScenes(SceneConfigs.OneSceneConfig);
+            }
+
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                _world.LoadScenes(SceneConfigs.TwoSceneConfig, true);
+            }
+
+            if (Input.GetKeyUp(KeyCode.Q))
+            {
+                _world.LoadScenes(SceneConfigs.BothScenesConfig, true);
+            }
+
+            if (Input.GetKeyUp(KeyCode.R))
+            {
+                _world.UnloadAllScenes();
+            }
+        }
+    }
+}
---
+++ StubbExample/Assets/Source/SceneConfigs.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using StubbFramework.Scenes.Configurations;
+using StubbUnity.Scenes;
+
+namespace Source
+{
+    public static  class SceneConfigs
+    {
+        public static readonly List<ILoadingSceneConfig> OneSceneConfig;
+        public static readonly List<ILoadingSceneConfig> TwoSceneConfig;
+        public static readonly List<ILoadingSceneConfig> BothScenesConfig;
+        
+        static SceneConfigs()
+        {
+            OneSceneConfig = SceneConfigsBuilder<LoadingSceneConfig, SceneName>.Create
+                .Add("One", "Scenes", true, true)
+                .Build;
+            
+            TwoSceneConfig = SceneConfigsBuilder<LoadingSceneConfig, SceneName>.Create
+                .Add("Two", "Scenes", true, true)
+                .Build;
+            
+            BothScenesConfig = SceneConfigsBuilder<LoadingSceneConfig, SceneName>.Create
+                .Add("One", "Scenes")
+                .Add("Two", "Scenes", true, true)
+                .Build;
+        }
+    }
+}
