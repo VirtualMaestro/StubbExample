9711834c615087a378c59a75fd22bfb3d3afe765
VirtualMaestro <virtualmaestro@gmail.com>
2020-04-23 20:09:04 +0200

    Upd

--- StubbExample/Assets/Source/SceneConfigs.cs
+++ StubbExample/Assets/Source/SceneConfigs.cs
@@ -1,43 +1,42 @@
 ﻿using System.Collections.Generic;
-using StubbFramework.Common.Names;
 using StubbFramework.Scenes.Configurations;
 using StubbUnity.Scenes;
 
 namespace Source
 {
-    public static  class SceneConfigs
+    public static class SceneConfigs
     {
         public static readonly SceneName CameraSceneName;
         public static readonly SceneName OneSceneName;
         public static readonly SceneName TwoSceneName;
-        
+
         public static readonly List<ILoadingSceneConfig> CameraSceneConfigs;
         public static readonly List<ILoadingSceneConfig> OneSceneConfigs;
         public static readonly List<ILoadingSceneConfig> TwoSceneConfigs;
         public static readonly List<ILoadingSceneConfig> AllScenesConfigs;
-        
+
         static SceneConfigs()
         {
             CameraSceneName = new SceneName("Camera", "Scenes");
             OneSceneName = new SceneName("One", "Scenes");
             TwoSceneName = new SceneName("Two", "Scenes");
-            
+
             CameraSceneConfigs = SceneConfigsBuilder<LoadingSceneConfig, SceneName>.Create
                 .Add(CameraSceneName).IsActive()
                 .Build;
-            
+
             OneSceneConfigs = SceneConfigsBuilder<LoadingSceneConfig, SceneName>.Create
                 .Add(OneSceneName).IsActive().IsMain()
                 .Build;
-            
+
             TwoSceneConfigs = SceneConfigsBuilder<LoadingSceneConfig, SceneName>.Create
                 .Add(TwoSceneName).IsActive()
                 .Build;
-            
+
             AllScenesConfigs = SceneConfigsBuilder<LoadingSceneConfig, SceneName>.Create
                 .Add(CameraSceneName)
                 .Add(OneSceneConfigs[0]).IsMain(false)
-                .Add(TwoSceneName).IsMain()
+                .Add(TwoSceneName).IsActive().IsMain().IsMultiple()
                 .Build;
         }
     }
