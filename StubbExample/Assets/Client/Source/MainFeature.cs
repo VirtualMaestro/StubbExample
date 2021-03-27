23101dccbae697f2bd82df5c7eac95ec5f5b0646
VirtualMaestro <virtualmaestro@gmail.com>
2021-03-27 12:33:27 +0100

    Update StubbUnity. Load some scene after the app starts.

--- StubbExample/Assets/Client/Source/MainFeature.cs
+++ StubbExample/Assets/Client/Source/MainFeature.cs
@@ -1,14 +1,23 @@
 ﻿using Client.Source.UI;
+using Leopotam.Ecs;
 using StubbUnity.StubbFramework.Core;
+using StubbUnity.StubbFramework.Extensions;
 
 namespace Client.Source
 {
-    public class MainFeature : EcsFeature
+    public class MainFeature : EcsFeature, IEcsInitSystem
     {
+        private EcsWorld _world;
+        
         public MainFeature()
         {
             Add(new ScenesManagerSystem());
             Add(new UIMenuFeature());
+        }
+
+        public void Init()
+        {
+            _world.LoadScenes(SceneConfigs.CameraSceneConfigs, true);
         }
     }
 }
